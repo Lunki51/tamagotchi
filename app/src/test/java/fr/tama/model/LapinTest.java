83114721d5d2d70ac9af1c76ff808a8cf7ad9e0d
Alix Regnier <tankermetal@gmail.com>
2021-11-21 17:37:10 +0000

    Test unitaire class Lapin

---
+++ app/src/test/java/fr/tama/model/LapinTest.java
@@ -0,0 +1,100 @@
+package fr.tama.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LapinTest {
+
+    static Lapin lapin;
+    static final Status MOOD = Status.VERY_GOOD;
+    static final Status SHAPE = Status.VERY_GOOD;
+    static final Current CURRENT = Current.AWAKE;
+    static final boolean SEX = true;
+    static final String NAME = "Carrot";
+
+    @Before
+    public void init()
+    {
+        lapin = new Lapin(MOOD, SHAPE, CURRENT, SEX, NAME);
+    }
+
+    @After
+    public void destroy()
+    {
+        //Le coup du lapin
+        lapin.setCurrent(Current.DEAD);
+    }
+
+    @Test
+    public void testInit()
+    {
+        assertEquals(lapin.getMood(), MOOD);
+        assertEquals(lapin.getShape(), SHAPE);
+        assertEquals(lapin.getCurrent(), CURRENT);
+        assertEquals(lapin.isSex(), SEX);
+        assertEquals(lapin.getName(), NAME);
+    }
+
+    @Test
+    public void testEat()
+    {
+        //Need more information about action
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        int stat = lapin.getAttribute("hunger").getValue();
+        lapin.eat();
+        assertTrue(stat > lapin.getAttribute("hunger").getValue());
+    }
+
+    @Test
+    public void testSleep()
+    {
+        //Need more information about action
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        int stat = lapin.getAttribute("tiredness").getValue();
+        lapin.sleep();
+        assertTrue(stat > lapin.getAttribute("tiredness").getValue());
+    }
+
+    @Test
+    public void testPlay()
+    {
+        //Need more information about action
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        int stat = lapin.getAttribute("hapiness").getValue();
+        lapin.play();
+        assertTrue(stat < lapin.getAttribute("hapiness").getValue());
+    }
+
+    @Test
+    public void testToilet()
+    {
+        //Need more information about action
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        int stat = lapin.getAttribute("toilet").getValue();
+        lapin.toilet();
+        assertTrue(stat > lapin.getAttribute("toilet").getValue());
+    }
+
+    @Test
+    public void testWash()
+    {
+        //Need more information about action
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        assertNotEquals(lapin.getCurrent(), Current.DEAD);
+        int stat = lapin.getAttribute("cleanliness").getValue();
+        lapin.wash();
+        assertTrue(stat < lapin.getAttribute("cleanliness").getValue());
+    }
+
+    @Test
+    public void testUpdate() //TODO
+    {
+        //Need more information about action
+    }
+}

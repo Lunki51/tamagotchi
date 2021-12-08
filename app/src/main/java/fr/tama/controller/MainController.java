cb9b425123bf91e67053a49d331d6b648fcb6095
Navet56 <evandib@gmail.com>
2021-12-08 17:40:57 +0100

    Fix Main class

--- app/src/main/java/fr/tama/controller/MainController.java
+++ app/src/main/java/fr/tama/controller/MainController.java
@@ -3,7 +3,7 @@ package fr.tama.controller;
 import javax.swing.*;
 
 public class MainController {
-    public static void main() {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(()-> {
         GameController controller = new GameController();
         controller.startGame();

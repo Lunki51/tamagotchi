4f5cd59ac8435dc3a60be06c4e8b7d3efa60495b
Navet56 <evandib@gmail.com>
2021-12-22 18:07:08 +0100

    Fix de la langue

---
+++ app/src/main/java/fr/tama/model/Constants.java
@@ -0,0 +1,10 @@
+package fr.tama.model;
+
+import java.awt.*;
+
+public class Constants {
+    public final static Color BLUE = new Color(126,206,202);
+    public final static Color PURPLE = new Color(130,3,120);
+    public final static Font BASIC_FONT = new Font(Font.SANS_SERIF,  Font.BOLD, 20);
+    public final static Font TITLE_FONT = new Font(Font.SANS_SERIF,  Font.BOLD, 30);
+}

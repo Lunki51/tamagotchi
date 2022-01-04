54d03dafb28a6e6b94c34b64355024f1c04e3e3b
Lunki <quentincosnier@free.fr>
2022-01-04 19:06:20 +0100

    Doc ajours

--- app/src/main/java/fr/tama/view/utils/Animation.java
+++ app/src/main/java/fr/tama/view/utils/Animation.java
@@ -3,6 +3,9 @@ package fr.tama.view.utils;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * Abstract class that represent an animation in the game
+ */
 public abstract class Animation implements Runnable {
 
     long delta;
@@ -10,13 +13,25 @@ public abstract class Animation implements Runnable {
     protected Thread myThread;
     protected final ArrayList<ActionListener> listeners = new ArrayList<>();
 
+    /**
+     * Create a new animation
+     * @param delta the delta time between the beginning and the end of the animation
+     * @param nbLoop the number of loop for the animation
+     */
     public Animation(long delta, int nbLoop){
         this.delta = delta;
         this.nbLoop=nbLoop;
     }
 
+    /**
+     * Start the animation
+     */
     abstract public void start();
 
+    /**
+     * Add a listener to the animation
+     * @param l the listener to add
+     */
     public void addUpdateListener(ActionListener l){
         this.listeners.add(l);
     }
